c965afb09ded9dddf4898e36ca772cf6d83abc09
Gustavo Emiliano <gustavoemiliano2014@gmail.com>
2024-11-03 21:20:09 -0300

    add: utilizando ArrayList para listar filmes

--- src/Principal.java
+++ src/Principal.java
@@ -4,45 +4,63 @@ import br.com.alura.screenmatch.modelos.Episodio;
 import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 
+import java.util.ArrayList;
+
 public class Principal {
     public static void main(String[] args) {
-        Filme favorito = new Filme();
-        favorito.setNome("The Matrix");
-        favorito.setAnoDeLancamento(1999);
-        favorito.setDuracaoEmMinutos(135);
-        favorito.setIncluidoNoPlano(true);
+        Filme matrix = new Filme();
+        matrix.setNome("The Matrix");
+        matrix.setAnoDeLancamento(1999);
+        matrix.setDuracaoEmMinutos(135);
+        matrix.setIncluidoNoPlano(true);
+        matrix.avalia(10);
 
-        Filme outro = new Filme();
-        outro.setNome("John Wick");
-        outro.setAnoDeLancamento(2014);
-        outro.setDuracaoEmMinutos(101);
-        outro.setIncluidoNoPlano(true);
+        Filme johnWick = new Filme();
+        johnWick.setNome("John Wick");
+        johnWick.setAnoDeLancamento(2014);
+        johnWick.setDuracaoEmMinutos(101);
+        johnWick.setIncluidoNoPlano(true);
+        johnWick.avalia(9);
 
-        Serie serie = new Serie();
-        serie.setNome("La Casa de Papel");
-        serie.setAnoDeLancamento(2017);
-        serie.setIncluidoNoPlano(true);
-        serie.setAtiva(true);
-        serie.setTemporadas(5);
-        serie.setEpisodiosPorTemporada(10);
-        serie.setMinutospPorEpisodio(45);
+        Filme avatar = new Filme();
+        avatar.setNome("Avatar");
+        avatar.setAnoDeLancamento(2012);
+        avatar.setDuracaoEmMinutos(400);
+        avatar.setIncluidoNoPlano(true);
+        avatar.avalia(10);
+
+        Serie laCasaDePapel = new Serie();
+        laCasaDePapel.setNome("La Casa de Papel");
+        laCasaDePapel.setAnoDeLancamento(2017);
+        laCasaDePapel.setIncluidoNoPlano(true);
+        laCasaDePapel.setAtiva(true);
+        laCasaDePapel.setTemporadas(5);
+        laCasaDePapel.setEpisodiosPorTemporada(10);
+        laCasaDePapel.setMinutospPorEpisodio(45);
 
         CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
-        calculadora.inclui(favorito);
-        calculadora.inclui(outro);
-        calculadora.inclui(serie);
+        calculadora.inclui(matrix);
+        calculadora.inclui(johnWick);
+        calculadora.inclui(laCasaDePapel);
 
         System.out.println("Tempo total: " +calculadora.getTempoTotal());
 
         FiltroRecomendacao filtro = new FiltroRecomendacao();
-        filtro.filtra(favorito);
+        filtro.filtra(matrix);
 
         Episodio episodio = new Episodio();
         episodio.setNumero(1);
-        episodio.setSerie(serie);
+        episodio.setSerie(laCasaDePapel);
         episodio.setTotalAvaliacoes(300);
         filtro.filtra(episodio);
 
+
+        ArrayList<Filme> listaDeFilmes = new ArrayList<>();
+        listaDeFilmes.add(matrix);
+        listaDeFilmes.add(johnWick);
+        listaDeFilmes.add(avatar);
+        System.out.println("Tamanho da lista: " + listaDeFilmes.size());
+        System.out.println("Primeiro filme: " +listaDeFilmes.get(0).getNome());
     }
 
 }
--- src/br/com/alura/screenmatch/calculos/CalculadoraDeTempo.java
+++ src/br/com/alura/screenmatch/calculos/CalculadoraDeTempo.java
@@ -18,7 +18,7 @@ public class CalculadoraDeTempo {
 //    }
     public void inclui(Titulo titulo){
         this.tempoTotal += titulo.getDuracaoEmMinutos();
-        System.out.println("Adicionando duração em minutos de: " + titulo);
+        System.out.println("Adicionando duração em minutos de: " + titulo.getNome());
     }
 
 }
